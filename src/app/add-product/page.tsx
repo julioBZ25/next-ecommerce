296a8bab1987d8f22e81ce066e94d5b396cf1652
Julio <jbzuloaga@gmail.com>
2023-07-06 12:52:35 -0500

    Add add product page with server action

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,8 +1,34 @@
+import FormSubmitButton from "@/components/FormSubmitButton";
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+  title: "Add Product - Hyusecommerce",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageURL = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageURL || !price)
+    throw Error("Missing required field");
+
+  await prisma.product.create({
+    data: { name, description, imageURL, price },
+  });
+
+  redirect("/");
+}
+
 export default function AddProductPage() {
   return (
     <div>
       <h1 className="mb-3 text-xl font-bold">Add Product</h1>
-      <form>
+      <form action={addProduct}>
         <input
           required
           name="name"
@@ -30,9 +56,9 @@ export default function AddProductPage() {
           type="number"
           className="input-bordered input mb-3 w-full"
         />
-        <button type="submit" className="btn-primary btn-block btn">
+        <FormSubmitButton type="submit" className="btn-block">
           Add Product
-        </button>
+        </FormSubmitButton>
       </form>
     </div>
   );
