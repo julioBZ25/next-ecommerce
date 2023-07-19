4270918be03fc1753e0750474ae8bdd9fa125687
Julio <jbzuloaga@gmail.com>
2023-07-19 11:18:44 -0500

    Add merge anon cart with user session

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,20 +2,32 @@ import { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/db/prisma";
 import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth/next";
 import { env } from "@/lib/env";
+import { mergeAnonCartIntoUserCart } from "@/lib/db/cart";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET
-    })
-  ]
-}
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  events: {
+    async signIn({ user }) {
+      await mergeAnonCartIntoUserCart(user.id);
+    },
+  },
+};
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST };
